7ad28680e9c18b61a0f6ededf68c720af6c93406
Howzen <Howard_empire123@hotmail.com>
2013-08-13 00:46:21 +1000

    Added Primary Key. Errors Appearing due to Foreign Key which I am still trying to fix and code key

--- JMH123_schema.sql
+++ JMH123_schema.sql
@@ -8,22 +8,22 @@ DROP SCHEMA JMH123 RESTRICT;
 CREATE SCHEMA JMH123;
 
 CREATE TABLE JMH123.Employee(
-E_ID varchar(5), E_FirstName varchar(255),
+E_ID varchar(5) NOT null PRIMARY KEY, E_FirstName varchar(255),
 E_LastName varchar(255), E_Password varchar(255)
 );
 
 CREATE TABLE JMH123.Customer(
-C_ID varchar(5), C_FirstName varchar(255),
+C_ID varchar(5) NOT null PRIMARY KEY, C_FirstName varchar(255),
 C_LastName varchar(255), DOB date, Address varchar (255)
 );
 
 CREATE TABLE JMH123.Savings(
-C_ID varchar(5), AccNum varchar(255), Balance decimal(6,2),
-S_DateTimeCreated timestamp
+C_ID varchar(5), AccNum varchar(255) Not null PRIMARY KEY, Balance decimal(6,2),
+S_DateTimeCreated timestamp, FOREIGN KEY (C_ID) REFERENCES JMH123.Customer(C_ID)
 );
 
 CREATE TABLE JMH123.Transactions(
-T_ID varchar(5), AccNum varchar(255), Amount decimal(6,2),
+T_ID varchar(5) Not null PRIMARY KEY, AccNum varchar(255), Amount decimal(6,2),
 Description varchar(255), AccNum2 varchar(255), E_ID varchar(5),
-T_DateTimeCreated timestamp
+T_DateTimeCreated timestamp, FOREIGN KEY (AccNum) REFERENCES JMH123.Savings(AccNum)
 );
