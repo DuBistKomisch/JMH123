86c470df05db96b7fab58b7eddf54633e6c0b768
Howzen <Howard_empire123@hotmail.com>
2013-08-15 12:55:09 +1000

    Final Database 1.1 (Added "not nulls", got errors)

---
+++ ACME_BankingSystem/ACME_BankingSystem-ejb/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+
+/**
+ *
+ * @author Howard Tseng
+ */
+public class Customer implements CustomerDAO{
+    
+    private String C_ID;
+    private String C_FirstName;
+    private String C_LastName;
+    /*Need to replace Date data type*/
+    private Date DOB;
+    private String Address;
+    
+
+}
--- JMH123_schema.sql
+++ JMH123_schema.sql
@@ -1,45 +1,45 @@
-
-/*DROP TABLE JMH123.Transactions;
+/*
+DROP TABLE JMH123.Transactions;
 DROP TABLE JMH123.Employees;
 DROP TABLE JMH123.Savings;
-DROP TABLE JMH123.Customers;
+DROP TABLE JMH123.Customer;
 
 DROP SCHEMA JMH123 RESTRICT;
-*/
 
+*/
 CREATE SCHEMA JMH123;
 
 CREATE TABLE JMH123.Employees (
     E_ID varchar(5) NOT null PRIMARY KEY,
-    E_FirstName varchar(255),
-    E_LastName varchar(255),
-    E_Password varchar(255)
+    E_FirstName varchar(255) NOT NULL,
+    E_LastName varchar(255) NOT NULL,
+    E_Password varchar(255) NOT NULL
 );
 
-CREATE TABLE JMH123.Customers (
+CREATE TABLE JMH123.Customer (
     C_ID varchar(5) NOT null PRIMARY KEY,
-    C_FirstName varchar(255),
-    C_LastName varchar(255),
-    DOB date,
-    Address varchar(255)
+    C_FirstName varchar(255) NOT NULL,
+    C_LastName varchar(255) NOT NULL,
+    DOB date NOT NULL,
+    Address varchar(255) NOT NULL
 );
 
 CREATE TABLE JMH123.Savings (
     C_ID varchar(5),
     AccNum varchar(255) NOT null PRIMARY KEY,
-    Balance decimal(6,2),
-    S_DateTimeCreated timestamp,
+    Balance decimal(6,2) NOT null,
+    S_DateTimeCreated timestamp NOT null,
     FOREIGN KEY (C_ID) REFERENCES JMH123.Customers (C_ID)
 );
 
 CREATE TABLE JMH123.Transactions (
     T_ID varchar(5) Not null PRIMARY KEY,
     AccNum varchar(255),
-    Amount decimal(6,2),
+    Amount decimal(6,2) NOT NULL,
     Description varchar(255),
     AccNum2 varchar(255),
     E_ID varchar(5),
-    T_DateTimeCreated timestamp,
+    T_DateTimeCreated timestamp NOT NULL,
     FOREIGN KEY (E_ID) REFERENCES JMH123.Employees (E_ID),
     FOREIGN KEY (AccNum) REFERENCES JMH123.Savings (AccNum)
 );
